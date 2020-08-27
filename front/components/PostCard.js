54e79db19b372a5db123998b7bc090b4741a4fc4
sbinha0819 <sbinha123@gmail.com>
2020-08-27 12:08:41 +0900

    in progress in redux

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Popover, Button, Avatar } from 'antd';
+import { useSelector } from 'react-redux';
+import {
+  RetweetOutlined,
+  HeartTwoTone,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+} from '@ant-design/icons';
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+import PostImages from './PostImages';
+
+const PostCard = ({ post }) => {
+  const id = useSelector((state) => state.user.me?.id);
+
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key='retweet' />,
+          <HeartOutlined key='heart' />,
+          <MessageOutlined key='comment' />,
+          <Popover
+            key='more'
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button type=''>수정</Button>
+                    <Button type='danger'>삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+      </Card>
+      {/* <CommentForm />
+      <Comments /> */}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.object,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
 };
 
 export default PostCard;
