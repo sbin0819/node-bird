df40001d40b10d09428e80975db8add79b4eb25e
sbinha0819 <sbinha123@gmail.com>
2020-10-04 22:04:18 +0900

    edit reducers, sagas

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -2,6 +2,15 @@ import { all, fork, takeLatest, delay, put } from 'redux-saga/effects';
 
 import axios from 'axios';
 
+import {
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  ADD_POST_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_COMMENT_FAILURE,
+} from '../reducers/post';
+
 function addPostAPI(data) {
   return axios.post('/api/post', data);
 }
@@ -11,21 +20,45 @@ function* addPost(action) {
     yield delay(1000);
     // const result = yield call(addPostAPI, action.data);
     yield put({
-      type: 'ADD_POST_SUCCESS',
+      type: ADD_POST_SUCCESS,
       // data: result.data,
     });
   } catch (err) {
     yield put({
-      type: 'ADD_POST_FAILURE',
-      data: err.response.data,
+      type: ADD_POST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function addCommentAPI(data) {
+  return axios.post('/api/comment', data);
+}
+
+function* addComment(action) {
+  try {
+    yield delay(1000);
+    // const result = yield call(addCommentAPI, action.data);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      // data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      error: err.response.data,
     });
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -2,6 +2,18 @@ import { all, fork, takeLatest, delay, put } from 'redux-saga/effects';
 
 import axios from 'axios';
 
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+  LOG_OUT_REQUEST,
+  LOG_OUT_SUCCESS,
+  LOG_OUT_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE,
+} from '../reducers/user';
+
 function logInAPI(data) {
   return axios.post('/api/login', data);
 }
@@ -11,14 +23,14 @@ function* logIn(action) {
     // const result = yield call(logInAPI, action.data);
     yield delay(1000);
     yield put({
-      type: 'LOG_IN_SUCCESS',
+      type: LOG_IN_SUCCESS,
       // data: result.data,
       data: action.data,
     });
   } catch (err) {
     yield put({
-      type: 'LOG_IN_FAILURE',
-      data: err.response.data,
+      type: LOG_IN_FAILURE,
+      error: err.response.data,
     });
   }
 }
@@ -32,25 +44,49 @@ function* logOut() {
     yield delay(1000);
     // const result = yield call(logOutAPI);
     yield put({
-      type: 'LOG_OUT_SUCCESS',
+      type: LOG_OUT_SUCCESS,
       // data: result.data,
     });
   } catch (err) {
     yield put({
-      type: 'LOG_OUT_FAILURE',
-      data: err.response.data,
+      type: LOG_OUT_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function signUpAPI() {
+  return axios.post('/api/signup');
+}
+
+function* signUp() {
+  try {
+    yield delay(1000);
+    // const result = yield call(signUpAPI);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+      // data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: err.response.data,
     });
   }
 }
 
 function* watchLogIn() {
-  yield takeLatest('LOG_IN_REQUEST', logIn);
+  yield takeLatest(LOG_IN_REQUEST, logIn);
 }
 
 function* watchLogOut() {
-  yield takeLatest('LOG_OUT_REQUEST', logOut);
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
-  yield all([fork(watchLogIn), fork(watchLogOut)]);
+  yield all([fork(watchLogIn), fork(watchLogOut), fork(watchSignUp)]);
 }
