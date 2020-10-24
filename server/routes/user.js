7d9c94bd17737d53f5afa3197fe78094bd864f88
sbinha0819 <sbinha123@gmail.com>
2020-10-24 14:14:54 +0900

    feat : add login middlewares

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const { User, Post } = require('../models');
-const db = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     // 미들웨어 확장
     if (err) {
@@ -43,7 +43,7 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', isNotLoggedIn, async (req, res, next) => {
   try {
     const exUser = await User.findOne({
       where: {
@@ -66,7 +66,7 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.post('/user/logout', (req, res, next) => {
+router.post('/logout', isLoggedIn, (req, res, next) => {
   req.logout();
   req.session.destroy();
   res.send('ok');
